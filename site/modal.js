f0f56e211a89fe42d0eee490f051405af00f29ae
Nick Pseudo <tomdop.dakar@gmail.com>
2024-04-25 23:35:35 +0200

    rename folder + first issue fix

---
+++ site/modal.js
@@ -0,0 +1,30 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalExitBtn = document.querySelector(".close");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// Close modal event
+modalExitBtn.addEventListener("click", closeModal);
+
+// Close modal function
+function closeModal() {
+    modalbg.style.display = "none";
+}
