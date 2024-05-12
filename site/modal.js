f71dd07b8261b1fe562735fb36de044289257f54
Tholho <163859510+Tholho@users.noreply.github.com>
2024-05-12 16:14:21 +0200

    Fix accolade

--- site/modal.js
+++ site/modal.js
@@ -1,6 +1,6 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") 
+  var x = document.getElementById("myTopnav"); 
+  if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
